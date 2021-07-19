aed3e12210a4487c01d23ec066e67346930387a9
Timur Badretdinov <destinerx@gmail.com>
2021-07-19 17:44:55 +0300

    Filter tokens by chain id

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,16 +1,29 @@
 import { writeFile, rm, mkdir } from 'fs/promises';
 import axios from 'axios';
 
-interface List {
+interface TokenListMeta {
 	id: string;
 	url: string;
+}
+
+interface TokenList {
+	name: string;
+	tokens: Token[];
+}
+
+interface Token {
+	chainId: number;
+	address: string;
+	name: string;
+	symbol: string;
+	decimals: number;
 }
 
 const chains: Record<number, string> = {
 	1: 'ethereum',
 }
 
-const lists: Record<number, List[]> = {
+const lists: Record<number, TokenListMeta[]> = {
 	1: [{
 		id: 'coingecko',
 		url: 'https://tokens.coingecko.com/uniswap/all.json',
@@ -45,7 +58,7 @@ async function run() {
 		const chainLists = lists[chainId];
 		for (const list of chainLists) {
 			const { id, url } = list;
-			const listData = await getList(url);
+			const listData = await getList(url, chainId);
 			const chain = chains[chainId];
 			const fileName = `${chain}:${id}.json`;
 			const filePath = `${LIST_DIR}/${fileName}`;
@@ -55,7 +68,7 @@ async function run() {
 	}
 }
 
-async function getList(url: string) {
+async function getList(url: string, chainId: number) {
 	let delay = 100;
 	while(true) {
 		await sleep(delay);
@@ -65,7 +78,12 @@ async function getList(url: string) {
 			if (listResponse.status !== 200) {
 				continue;
 			}
-			return listResponse.data;
+			const fullList = listResponse.data as TokenList;
+			const list = {
+				...fullList,
+				tokens: fullList.tokens.filter(token => token.chainId === chainId),
+			};
+			return list;
 		} catch(e) {
 			continue;
 		}
